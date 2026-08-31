57a9289f6927906855747eb2aeb664cb739430e6
almar obina <almarobina35@gmail.com>
2026-09-01 07:29:18 +0800

    Add exercise 22 cash payment calculato

---
+++ 22-cash-payment/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Item price: ");
+	decimal itemPrice = decimal.Parse(Console.ReadLine()!);
+
+Console.Write("Cash paid: ");
+	 decimal cashPaid = decimal.Parse(Console.ReadLine()!);
+
+
+
+if (itemPrice <= 0)
+{
+	Console.WriteLine("INVALID PRICE");
+}
+else if(cashPaid <= 0)
+{
+	Console.WriteLine("INVALID PAYMENT");
+}
+else if (cashPaid < itemPrice)
+{
+	Console.WriteLine("INSUFFICIENT PAYMENT");
+}
+else if (cashPaid == itemPrice)
+{
+	Console.WriteLine("PAYMENT SUCCESSFUL");
+	Console.WriteLine("Exact payment");
+	
+}
+else
+{
+	decimal change = cashPaid - itemPrice;
+
+	Console.WriteLine("PAYMENT SUCCESSFUL");
+	Console.WriteLine($"Change: ₱{change:N2}");
+}
