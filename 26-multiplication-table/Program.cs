69f6691f84e9a33f4dccbffc069ee40c4e7b723d
almar obina <almarobina35@gmail.com>
2026-09-03 20:11:47 +0800

    Add 26 multiplication table

---
+++ 26-multiplication-table/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter number: ");
+int numbers = int.Parse(Console.ReadLine()!);
+
+
+if (numbers <= 0)
+{
+	Console.WriteLine("INVALID NUMBER");
+}
+else 
+{
+	int i = 1;
+	while (i <= 10)
+	{
+		int products = numbers * i;
+		
+		
+	
+	Console.WriteLine($"{numbers} X {i} = {products}");
+	i++;
+	}
+}
