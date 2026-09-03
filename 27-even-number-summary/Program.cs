ba40967863b68ba2b2f10fba9aa2a802050c5fe9
almar obina <almarobina35@gmail.com>
2026-09-03 20:54:03 +0800

    Add 27 even number summary

---
+++ 27-even-number-summary/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number: ");
+int numbers = int.Parse(Console.ReadLine()!);
+
+
+if (numbers <= 0)
+{
+	Console.WriteLine("INVALID NUMBER");
+}
+else
+{
+	int i = 1;
+	int total = 0;
+	int count = 0;
+
+	while (i <= numbers)
+	{
+		if(i % 2 == 0)
+		{
+			total += i;
+			count++;
+			
+		}
+		i++;
+
+	}
+	Console.WriteLine($"Even sum: {total}");
+	Console.WriteLine($"Even count: {count}");
+}
