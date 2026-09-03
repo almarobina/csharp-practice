7aada334c3f958e49327b3ebd95bf4b2db04232a
almar obina <almarobina35@gmail.com>
2026-09-04 07:14:12 +0800

    Add 28 odd number summary

---
+++ 28-odd-number-summary/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter number ");
+int numbers = int.Parse(Console.ReadLine()!);
+
+if(numbers <= 0)
+{
+	Console.WriteLine("INVALID NUMBER");
+}
+else
+{
+	int i = 1;
+	int total = 0;
+	int count = 0;
+	while (i <= numbers)
+	{
+		if(i % 2 != 0)
+		{
+			total += i;
+			count++;
+		}
+		i++;
+	}
+	Console.WriteLine($"Odd count: {count}");
+	Console.WriteLine($"Odd sum: {total}");
+}
