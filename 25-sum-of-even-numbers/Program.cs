6d2a25497c74e9a3bba788faf2bfd6195ae7c382
almar obina <almarobina35@gmail.com>
2026-09-03 19:16:59 +0800

    Add 25 sum of even numbers

---
+++ 25-sum-of-even-numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿// See htt
+Console.Write("Enter number: ");
+int numbers = int.Parse(Console.ReadLine()!);
+
+
+if (numbers <= 0)
+{
+	Console.WriteLine("INVALID NUMBER");
+}
+else
+{
+
+int i = 1;
+int total = 0;
+
+	while (i <= numbers)
+	{
+	
+		if(i % 2 == 0)
+		{
+			total = total + i;
+		
+		}
+
+		i++;
+	}
+
+	Console.WriteLine($"Total: {total}");
+}
+
